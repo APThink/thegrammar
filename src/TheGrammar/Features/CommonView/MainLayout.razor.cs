193c3c38b3b3891cb36fbb5bbfa5c0519a4314fa
Alex Pinneker <pinneker7@gmail.com>
2026-03-29 22:07:52 +0200

    #26: Add green as primary and change button save to primary

--- src/TheGrammar/Features/CommonView/MainLayout.razor.cs
+++ src/TheGrammar/Features/CommonView/MainLayout.razor.cs
@@ -1,5 +1,20 @@
+using MudBlazor;
+
 namespace TheGrammar.Features.CommonView;
 
 public partial class MainLayout
 {
+    private MudTheme _theme = new()
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = "#5cb85c",
+            PrimaryContrastText = "#ffffff",
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = "#5cb85c",
+            PrimaryContrastText = "#ffffff",
+        }
+    };
 }
